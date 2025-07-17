5d8e1b25b013a30e60acb5dedee9bf9ca7bb6c96
juwel Sheikh <jewelshekh1996@gmail.com>
2025-07-18 01:21:30 +0600

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "989a3854a3ae9a7164e71b69e1b8c544",
+const RESOURCES = {"flutter_bootstrap.js": "c549952e917ee3a834dd35f4f199dc3b",
 "version.json": "104143d64207ce8cfa9a8aef55de5085",
 "index.html": "ad3c5652250aed2ba4a98c5df22476a4",
 "/": "ad3c5652250aed2ba4a98c5df22476a4",
-"main.dart.js": "5304ceae4ad6204d75c02e2aac7bf94b",
+"main.dart.js": "696ba692107a069ff2ebd6137c909175",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
